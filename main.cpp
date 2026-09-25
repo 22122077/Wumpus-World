8f6471a830d59ca969e8a29ffc5f8a90b8791080
Mina Armijo <22122077@student.westernsydney.edu.au>
2026-09-25 16:12:49 +1000

    Adding Direction enum

--- main.cpp
+++ main.cpp
@@ -13,9 +13,17 @@ struct Cell {
     bool hasGold = false;
 };
 
+enum class Direction {
+    North,
+    East,
+    South,
+    West
+};
+
 int main() {
     Position start{1, 1};
     Cell cell;
+    Direction startDirection = Direction::East;
 
     cout << "[" << start.x << "," << start.y << "]" << endl;
     cout << cell.hasPit << endl;
