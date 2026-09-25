0f95498912ac9abb059afedb3da943e5fe0b903a
Mina Armijo <22122077@student.westernsydney.edu.au>
2026-09-25 16:09:17 +1000

    Adding Cell struct

--- main.cpp
+++ main.cpp
@@ -6,9 +6,19 @@ struct Position {
     int x;
     int y;
 };
+
+struct Cell {
+    bool hasPit = false;
+    bool hasWumpus = false;
+    bool hasGold = false;
+};
+
 int main() {
     Position start{1, 1};
+    Cell cell;
 
     cout << "[" << start.x << "," << start.y << "]" << endl;
+    cout << cell.hasPit << endl;
+
     return 0;
 }
